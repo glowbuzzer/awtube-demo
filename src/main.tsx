fd51a1b9d559c9399e2bd974a3d8b64ce1251643
jugglingcats <github@akirkpatrick.com>
2023-11-06 15:03:01 +0000

    chore: remove local config

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,11 @@ import { App } from "./App"
 import "antd/dist/reset.css"
 import "dseg/css/dseg.css"
 import "flexlayout-react/style/light.css"
-import { config } from "./config"
 
 const root = createRoot(document.getElementById("root")!)
 
 root.render(
-    <GlowbuzzerApp appName={"myapp"} configuration={config}>
+    <GlowbuzzerApp appName="awtube-demo">
         <App />
     </GlowbuzzerApp>
 )
